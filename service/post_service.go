5f367f9893f14dde24cccd37ed0ae9481e35099a
Admin <b1716568@planet.kanazawa-it.ac.jp>
2019-07-03 05:58:26 +0900

    add: service init

---
+++ service/post_service.go
@@ -0,0 +1,41 @@
+package post
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/iruknuj/odaibox_API/db"
+	"github.com/iruknuj/odaibox_API/model"
+)
+
+type Service struct {
+}
+
+type Post model.Post
+
+// 全postの取得
+func (s Service) GetAll() ([]Post, error) {
+	db := db.GetDB()
+	var posts []Post
+
+	if err := db.Find(&posts).Error; err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+// Post modelの生成
+func (s Service) CreateModel(context *gin.Context) (Post, error) {
+	db := db.GetDB()
+	var posts Post
+
+	if err := context.BindJSON(&posts); err != nil {
+		return posts, err
+	}
+
+	if err := db.Create(&posts).Error; err != nil {
+		return posts, err
+	}
+
+	return posts, nil
+
+}
